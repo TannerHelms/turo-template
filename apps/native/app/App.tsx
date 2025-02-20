464759200bb6f7ef24b4b2bc800fdd8c1be9fddc
TannerHelms <tannerhelms01@gmail.com>
2025-02-20 00:25:37 -0700

    added in gluestack

--- apps/native/app/App.tsx
+++ apps/native/app/App.tsx
@@ -2,10 +2,14 @@ import SafeAreaView from '@ui/safe-area-view';
 import SignIn from '@views/sign-in';
 import "../global.css";
 import { Text } from 'react-native';
+import { Button, ButtonText } from "@ui/button"
 export default function App() {
   return (
     <SafeAreaView>
       <SignIn />
+      <Button>
+        <ButtonText>Sign In</ButtonText>
+      </Button>
     </SafeAreaView>
   );
 }
